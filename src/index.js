1bc079c251d0117fc2226b43b92818baad1abfa7
kasiraoyaradesi <you@example.com>
2024-03-05 14:36:59 +0530

    1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Coffe from './Coffe'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Coffe/>
   </React.StrictMode>
 );
 
